4c8e77eab7661b5e1b227a84ef88188a65128062
KEvin Wang <kvn.a.wng@gmail.com>
2013-08-05 01:19:23 -0700

    initial commit

---
+++ pyprototype.py
@@ -0,0 +1,14 @@
+#OOP python prototype of Fantasy Dota 2
+categories = ['k','d','a','gpm','xpm','lh','denies']
+
+def createNewLeague():
+    #choose categories
+    print "choose categories.  for example, if you want 5-cat, type 1111100"
+    newcategories = raw_input("'k','d','a','gpm','xpm','lh','denies'\n")
+    print newcategories
+    #choose player number
+    playernum = int(raw_input("enter the amount of players"))
+    #initiate
+
+def draft(league):
+    
