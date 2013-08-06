334f5f381fb2d48f11ba179237d79cfb6cabe093
KEvin Wang <kvn.a.wng@gmail.com>
2013-08-05 23:41:21 -0700

    added apikey config for python prototype

--- pyprototype.py
+++ pyprototype.py
@@ -1,14 +1,18 @@
 #OOP python prototype of Fantasy Dota 2
+import config
+apikey = config.apikey
 categories = ['k','d','a','gpm','xpm','lh','denies']
 
 def createNewLeague():
+    #choose points or cat
     #choose categories
     print "choose categories.  for example, if you want 5-cat, type 1111100"
     newcategories = raw_input("'k','d','a','gpm','xpm','lh','denies'\n")
     print newcategories
     #choose player number
     playernum = int(raw_input("enter the amount of players"))
+    #choose draft type (snake, etc)
     #initiate
 
 def draft(league):
-    
+    pass
